65a07fa2c8fa11115ca4cb688c452396fde011a2
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-11-16 23:26:55 +0300

    create sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import './App.scss';
+import {Route, Routes} from 'react-router-dom'
+import Layout from './components/Layout'
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Layout />} />
+    </Routes>
+    </>
   );
 }
 
