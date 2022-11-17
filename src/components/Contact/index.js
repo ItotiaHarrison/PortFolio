67824be75457e408d6cab6e26d2826cf7ce6810f
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-11-18 00:10:50 +0300

    make the contact form responsive

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,39 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
+    const refForm = useRef()
 
     useEffect(() => {
         return setTimeout(() => {
             setLetterClass('text-animate-hover')
         }, 3000)
     }, [])
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs
+        .sendForm(
+            'gmail',
+            'contact_form',
+            refForm.current,
+            'uM6jXG3uJnHNKQBjA'
+        )
+        .then(
+            () => {
+                alert('Message successfully sent!')
+                window.location.reload(false)
+            },
+            () => {
+                alert('Failed to send the message, please try again')
+            }
+        )
+    }
 
     return (
         <>
@@ -31,7 +54,7 @@ const Contact = () => {
 
                     </p>
                     <div className="contact-form">
-                        <form ref={form} onSubmit={sendEmail}>
+                        <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className="half">
                                     <input placeholder="Name" type="text" name="name" required />
