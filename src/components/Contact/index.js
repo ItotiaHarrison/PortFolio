bee591362770ee6b5db45ff986768e4b3352c287
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-11-17 23:05:59 +0300

    add contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,7 +5,7 @@ import './index.scss'
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
-    
+
     useEffect(() => {
         return setTimeout(() => {
             setLetterClass('text-animate-hover')
@@ -18,11 +18,53 @@ const Contact = () => {
                 <div className='text-zone'>
                     <h1>
                         <AnimatedLetters
-                        letterClass={letterClass}
+                            letterClass={letterClass}
                             strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
                             idx={15}
                         />
                     </h1>
+                    <p>
+
+                        I am interested in freelance opportunities - especially on ambitious
+                        or large projects. However, if you have any other requests or
+                        questions, don't hesitate to contact me using below form either.
+
+                    </p>
+                    <div className="contact-form">
+                        <form ref={form} onSubmit={sendEmail}>
+                            <ul>
+                                <li className="half">
+                                    <input placeholder="Name" type="text" name="name" required />
+                                </li>
+                                <li className="half">
+                                    <input
+                                        placeholder="Email"
+                                        type="email"
+                                        name="email"
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                    <input
+                                        placeholder="Subject"
+                                        type="text"
+                                        name="subject"
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                    <textarea
+                                        placeholder="Message"
+                                        name="message"
+                                        required
+                                    ></textarea>
+                                </li>
+                                <li>
+                                    <input type="submit" className="flat-button" value="SEND" />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
                 </div>
             </div>
             <Loader type='pacman' />
