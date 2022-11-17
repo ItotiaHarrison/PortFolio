7599fff294850cea96c23e81a8f201a6cfedb28f
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-11-17 14:15:59 +0300

    style about layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import {Route, Routes} from 'react-router-dom'
 import Layout from './components/Layout'
+import About from './components/About';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path='about' element={<About />} />
       </Route>
     </Routes>
     </>
